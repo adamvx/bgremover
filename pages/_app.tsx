edf37965cbdd089fd9adfec6284e49a049824de4
Adam Vician <adamvician@gmail.com>
2022-03-26 10:34:26 +0100

    Initial implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider
+			theme={createTheme({
+				palette: {
+					mode: "dark",
+				},
+			})}
+		>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="sk">
+				<Head>
+					<link rel="shortcut icon" href="/favicon.png" />
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+					/>
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/icon?family=Material+Icons"
+					/>
+				</Head>
+				<body className="antialiased scroll-bar">
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
