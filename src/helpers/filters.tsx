de9d416dbf01c79e1009d41729a16bef86870d21
Adam Vician <adamvician@gmail.com>
2022-03-27 19:24:29 +0200

    Fixed backgroud image filter

--- src/helpers/filters.tsx
+++ src/helpers/filters.tsx
@@ -1,63 +1,66 @@
 import { GpuBuffer } from "@mediapipe/selfie_segmentation";
 
 export interface IFilter {
-  apply(
-    webcam: GpuBuffer,
-    context: CanvasRenderingContext2D,
-    width: number,
-    height: number
-  ): void;
+	apply(
+		webcam: GpuBuffer,
+		context: CanvasRenderingContext2D,
+		width: number,
+		height: number
+	): void;
 }
 
 export class ColorFilter implements IFilter {
-  constructor(private color: string) {}
+	constructor(private color: string) {}
 
-  apply(
-    _: GpuBuffer,
-    context: CanvasRenderingContext2D,
-    width: number,
-    height: number
-  ): void {
-    context.globalCompositeOperation = "destination-atop";
-    context.fillStyle = this.color;
-    context.fillRect(0, 0, width, height);
-  }
+	apply(
+		_: GpuBuffer,
+		context: CanvasRenderingContext2D,
+		width: number,
+		height: number
+	): void {
+		context.globalCompositeOperation = "destination-atop";
+		context.fillStyle = this.color;
+		context.fillRect(0, 0, width, height);
+	}
 }
 
 export class BlurFilter implements IFilter {
-  constructor(private blur: number) {}
+	constructor(private blur: number) {}
 
-  apply(
-    webcam: GpuBuffer,
-    context: CanvasRenderingContext2D,
-    width: number,
-    height: number
-  ): void {
-    context.globalCompositeOperation = "destination-atop";
-    context.filter = `blur(${this.blur}px)`;
-    console.log("1");
+	apply(
+		webcam: GpuBuffer,
+		context: CanvasRenderingContext2D,
+		width: number,
+		height: number
+	): void {
+		context.globalCompositeOperation = "destination-atop";
+		context.filter = `blur(${this.blur}px)`;
+		console.log("1");
 
-    context.drawImage(webcam, 0, 0, width, height);
-  }
+		context.drawImage(webcam, 0, 0, width, height);
+	}
 }
 
-export class CustomBackgroundImage implements IFilter {
-  constructor(private image) {}
+export class CustomBackgroundImageFilter implements IFilter {
+	private img = new Image();
 
-  apply(
-    _: GpuBuffer,
-    context: CanvasRenderingContext2D,
-    width: number,
-    height: number
-  ): void {
-    const img = new Image();
-    img.src = URL.createObjectURL(this.image);
-    img.height = height;
-    img.width = width;
-    context.globalCompositeOperation = "destination-atop";
-    //context.fillStyle = this.image;
-    // context.fillRect(0, 0, width, height);
-    console.log(img);
-    context.drawImage(img, 0, 0, width, height);
-  }
+	constructor(private image: File) {
+		const fr = new FileReader();
+		fr.onload = () => {
+			this.img.src = fr.result as string;
+		};
+		fr.readAsDataURL(this.image);
+	}
+
+	apply(
+		_: GpuBuffer,
+		context: CanvasRenderingContext2D,
+		width: number,
+		height: number
+	): void {
+		this.img.height = height;
+		this.img.width = width;
+		context.globalCompositeOperation = "destination-atop";
+		context.drawImage(this.img, 0, 0, width, height);
+	}
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,109 +5,111 @@ import type { NextPage } from "next";
 import { useState } from "react";
 import { Webcam } from "../components/Webcam";
 import {
-  BlurFilter,
-  ColorFilter,
-  IFilter,
-  CustomBackgroundImage,
+	BlurFilter,
+	ColorFilter,
+	IFilter,
+	CustomBackgroundImageFilter,
 } from "../helpers/filters";
-import DeleteIcon from "@material-ui/icons/Delete";
 import BlurOnIcon from "@mui/icons-material/BlurOn";
 import FileUploadRoundedIcon from "@mui/icons-material/FileUploadRounded";
+import { DeleteOutlined } from "@mui/icons-material";
 
 const Container = styled.div`
-  min-width: 100vw;
-  min-height: 100vh;
-  padding: 16px;
-  display: flex;
-  flex-direction: row;
-  gap: 16px;
+	min-width: 100vw;
+	min-height: 100vh;
+	padding: 16px;
+	display: flex;
+	flex-direction: row;
+	gap: 16px;
 `;
 
 const VideoContainer = styled.div`
-  flex: 1;
-  display: flex;
-  align-self: center;
+	flex: 1;
+	display: flex;
+	align-self: center;
 `;
 
-const StyledButton = styled(Button)({
-  padding: "0px 30px",
-  height: "70px",
-  width: "70px",
-  margin: "2px",
-});
+const StyledButton = styled(Button)`
+	padding: 0px 30px;
+	height: 70px;
+	width: 70px;
+	margin: 2px;
+`;
 
 const Home: NextPage = () => {
-  const [filter, setFilter] = useState<IFilter>();
-  const [image, setImage] = useState(null);
+	const [filter, setFilter] = useState<IFilter>();
+	const [image, setImage] = useState<File | null>(null);
 
-  const handleFileUpload = (event) => {
-    console.log(event.target.files[0]);
-    setImage(event.target.files[0]);
-  };
-  return (
-    <Container>
-      <VideoContainer>
-        <Webcam filter={filter} />
-      </VideoContainer>
-      <Paper elevation={1} style={{ width: 384 }}>
-        <StyledButton
-          style={{ backgroundColor: "#ff0000" }}
-          onClick={() => setFilter(new ColorFilter("#ff0000"))}
-        ></StyledButton>
-        <StyledButton
-          style={{ backgroundColor: "#00ff00" }}
-          onClick={() => setFilter(new ColorFilter("#00ff00"))}
-        ></StyledButton>
-        <StyledButton
-          style={{ backgroundColor: "#0000ff" }}
-          onClick={() => setFilter(new ColorFilter("#0000ff"))}
-        ></StyledButton>
-        <StyledButton
-          style={{ backgroundColor: "#fff000" }}
-          onClick={() => setFilter(new ColorFilter("#fff000"))}
-        ></StyledButton>
-        <StyledButton
-          style={{
-            backgroundColor: "#dfdfdf",
-            flexDirection: "column",
-            fontSize: "7px",
-            color: "#9c9c9c",
-          }}
-          onClick={() => setFilter(new BlurFilter(6))}
-        >
-          <BlurOnIcon fontSize="large" /> light blur
-        </StyledButton>
-        <StyledButton
-          style={{
-            backgroundColor: "#dfdfdf",
-            flexDirection: "column",
-            fontSize: "7px",
-            color: "#9c9c9c",
-          }}
-          onClick={() => setFilter(new BlurFilter(12))}
-        >
-          <BlurOnIcon fontSize="large" /> strong blur
-        </StyledButton>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleFileUpload}
-          //style={{ display: "none" }}
-        />
-        <StyledButton
-          style={{ backgroundColor: "#dfdfdf", color: "#9c9c9c" }}
-          onClick={() => setFilter(new CustomBackgroundImage(image))}
-        >
-          <FileUploadRoundedIcon fontSize="large" />
-        </StyledButton>
-        <StyledButton
-          style={{ backgroundColor: "#dfdfdf", color: "#9c9c9c" }}
-          onClick={() => setFilter(undefined)}
-        >
-          <DeleteIcon />
-        </StyledButton>
+	const handleFileUpload = (files: FileList | null) => {
+		console.log(files?.[0]);
+		setImage(files?.[0] || null);
+	};
+	return (
+		<Container>
+			<VideoContainer>
+				<Webcam filter={filter} />
+			</VideoContainer>
+			<Paper elevation={1} style={{ width: 384 }}>
+				<StyledButton
+					style={{ backgroundColor: "#ff0000" }}
+					onClick={() => setFilter(new ColorFilter("#ff0000"))}
+				></StyledButton>
+				<StyledButton
+					style={{ backgroundColor: "#00ff00" }}
+					onClick={() => setFilter(new ColorFilter("#00ff00"))}
+				></StyledButton>
+				<StyledButton
+					style={{ backgroundColor: "#0000ff" }}
+					onClick={() => setFilter(new ColorFilter("#0000ff"))}
+				></StyledButton>
+				<StyledButton
+					style={{ backgroundColor: "#fff000" }}
+					onClick={() => setFilter(new ColorFilter("#fff000"))}
+				></StyledButton>
+				<StyledButton
+					style={{
+						backgroundColor: "#dfdfdf",
+						flexDirection: "column",
+						fontSize: "7px",
+						color: "#9c9c9c",
+					}}
+					onClick={() => setFilter(new BlurFilter(6))}
+				>
+					<BlurOnIcon fontSize="large" /> light blur
+				</StyledButton>
+				<StyledButton
+					style={{
+						backgroundColor: "#dfdfdf",
+						flexDirection: "column",
+						fontSize: "7px",
+						color: "#9c9c9c",
+					}}
+					onClick={() => setFilter(new BlurFilter(12))}
+				>
+					<BlurOnIcon fontSize="large" /> strong blur
+				</StyledButton>
+				<input
+					type="file"
+					accept="image/*"
+					onChange={({ target: { files } }) => handleFileUpload(files)}
+					style={{ display: "none" }}
+				/>
+				<StyledButton
+					style={{ backgroundColor: "#dfdfdf", color: "#9c9c9c" }}
+					onClick={() =>
+						image && setFilter(new CustomBackgroundImageFilter(image))
+					}
+				>
+					<FileUploadRoundedIcon fontSize="large" />
+				</StyledButton>
+				<StyledButton
+					style={{ backgroundColor: "#dfdfdf", color: "#9c9c9c" }}
+					onClick={() => setFilter(undefined)}
+				>
+					<DeleteOutlined />
+				</StyledButton>
 
-        {/* <Button onClick={() => setFilter(new ColorFilter("#ff0000"))}>
+				{/* <Button onClick={() => setFilter(new ColorFilter("#ff0000"))}>
           Red
         </Button>
         <Button onClick={() => setFilter(new ColorFilter("#00ff00"))}>
@@ -122,9 +124,9 @@ const Home: NextPage = () => {
         <Button onClick={() => setFilter(new BlurFilter(6))}>Blur</Button>
         <Button onClick={() => setFilter(new BlurFilter(12))}>Big Blur</Button>
         <Button onClick={() => setFilter(undefined)}>Clear</Button> */}
-      </Paper>
-    </Container>
-  );
+			</Paper>
+		</Container>
+	);
 };
 
 export default Home;
