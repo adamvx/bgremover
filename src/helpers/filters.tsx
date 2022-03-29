01dc4b4cf72bface10a69ccfac5f2c314f457b7a
danieloros1 <90513859+danieloros1@users.noreply.github.com>
2022-03-29 16:51:57 +0200

    added upload functionality

--- src/helpers/filters.tsx
+++ src/helpers/filters.tsx
@@ -1,66 +1,66 @@
 import { GpuBuffer } from "@mediapipe/selfie_segmentation";
-
+import { RcFile } from "rc-upload/lib/interface";
 export interface IFilter {
-	apply(
-		webcam: GpuBuffer,
-		context: CanvasRenderingContext2D,
-		width: number,
-		height: number
-	): void;
+  apply(
+    webcam: GpuBuffer,
+    context: CanvasRenderingContext2D,
+    width: number,
+    height: number
+  ): void;
 }
 
 export class ColorFilter implements IFilter {
-	constructor(private color: string) {}
+  constructor(private color: string) {}
 
-	apply(
-		_: GpuBuffer,
-		context: CanvasRenderingContext2D,
-		width: number,
-		height: number
-	): void {
-		context.globalCompositeOperation = "destination-atop";
-		context.fillStyle = this.color;
-		context.fillRect(0, 0, width, height);
-	}
+  apply(
+    _: GpuBuffer,
+    context: CanvasRenderingContext2D,
+    width: number,
+    height: number
+  ): void {
+    context.globalCompositeOperation = "destination-atop";
+    context.fillStyle = this.color;
+    context.fillRect(0, 0, width, height);
+  }
 }
 
 export class BlurFilter implements IFilter {
-	constructor(private blur: number) {}
+  constructor(private blur: number) {}
 
-	apply(
-		webcam: GpuBuffer,
-		context: CanvasRenderingContext2D,
-		width: number,
-		height: number
-	): void {
-		context.globalCompositeOperation = "destination-atop";
-		context.filter = `blur(${this.blur}px)`;
-		console.log("1");
+  apply(
+    webcam: GpuBuffer,
+    context: CanvasRenderingContext2D,
+    width: number,
+    height: number
+  ): void {
+    context.globalCompositeOperation = "destination-atop";
+    context.filter = `blur(${this.blur}px)`;
+    console.log("1");
 
-		context.drawImage(webcam, 0, 0, width, height);
-	}
+    context.drawImage(webcam, 0, 0, width, height);
+  }
 }
 
 export class CustomBackgroundImageFilter implements IFilter {
-	private img = new Image();
+  private img = new Image();
 
-	constructor(private image: File) {
-		const fr = new FileReader();
-		fr.onload = () => {
-			this.img.src = fr.result as string;
-		};
-		fr.readAsDataURL(this.image);
-	}
+  constructor(private image: RcFile) {
+    const fr = new FileReader();
+    fr.onload = () => {
+      this.img.src = fr.result as string;
+    };
+    fr.readAsDataURL(this.image);
+  }
 
-	apply(
-		_: GpuBuffer,
-		context: CanvasRenderingContext2D,
-		width: number,
-		height: number
-	): void {
-		this.img.height = height;
-		this.img.width = width;
-		context.globalCompositeOperation = "destination-atop";
-		context.drawImage(this.img, 0, 0, width, height);
-	}
+  apply(
+    _: GpuBuffer,
+    context: CanvasRenderingContext2D,
+    width: number,
+    height: number
+  ): void {
+    this.img.height = height;
+    this.img.width = width;
+    context.globalCompositeOperation = "destination-atop";
+    context.drawImage(this.img, 0, 0, width, height);
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,121 +9,115 @@ import { FilterItem } from "../components/FilterItem";
 import { FiltersGroup } from "../components/FiltersGroup";
 import { Webcam } from "../components/Webcam";
 import {
-	BlurFilter,
-	ColorFilter,
-	CustomBackgroundImageFilter,
-	IFilter,
+  BlurFilter,
+  ColorFilter,
+  CustomBackgroundImageFilter,
+  IFilter,
 } from "../helpers/filters";
 import { getVideoStream } from "../helpers/getVideoStream";
 import { getResolution } from "../helpers/resolution";
+import Upload from "rc-upload";
 
 const Container = styled.div`
-	min-width: 100vw;
-	min-height: 100vh;
-	padding: 16px;
-	display: flex;
-	flex-direction: row;
-	gap: 16px;
+  min-width: 100vw;
+  min-height: 100vh;
+  padding: 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
 `;
 
 const VideoContainer = styled.div`
-	flex: 1;
-	display: flex;
-	align-self: center;
+  flex: 1;
+  display: flex;
+  align-self: center;
 `;
 
 const Home: NextPage = () => {
-	const [filter, setFilter] = useState<IFilter>();
-	const [image, setImage] = useState<File | null>(null);
-	const [permissionStatus, setPermissionStatus] = useState<boolean>();
-	const resolution = getResolution("hd");
+  const [filter, setFilter] = useState<IFilter>();
+  const [permissionStatus, setPermissionStatus] = useState<boolean>();
+  const resolution = getResolution("hd");
 
-	const handleFileUpload = (files: FileList | null) => {
-		console.log(files?.[0]);
-		setImage(files?.[0] || null);
-	};
+  useEffect(() => {
+    getVideoStream(resolution.width, resolution.height)
+      .then(() => setPermissionStatus(true))
+      .catch(() => setPermissionStatus(false));
+  }, []);
 
-	useEffect(() => {
-		getVideoStream(resolution.width, resolution.height)
-			.then(() => setPermissionStatus(true))
-			.catch(() => setPermissionStatus(false));
-	}, []);
-
-	return (
-		<Container>
-			<VideoContainer>
-				{permissionStatus !== undefined && (
-					<Webcam filter={filter} resolution={resolution} />
-				)}
-			</VideoContainer>
-			<Paper elevation={1} style={{ width: 336 }}>
-				<FiltersGroup title="Filtre">
-					<FilterItem
-						tooltip="Odstrániť filtre"
-						icon={<BlockOutlined fontSize="large" />}
-						onClick={() => setFilter(undefined)}
-					/>
-					<FilterItem
-						tooltip="Ľahké rozmazanie"
-						icon={<DeblurOutlined fontSize="large" />}
-						onClick={() => setFilter(new BlurFilter(6))}
-					/>
-					<FilterItem
-						tooltip="Silné rozmazanie"
-						icon={<BlurOnIcon fontSize="large" />}
-						onClick={() => setFilter(new BlurFilter(12))}
-					/>
-				</FiltersGroup>
-				<FiltersGroup title="Farby">
-					<FilterItem
-						tooltip="Červené pozadie"
-						backgroundColor="#ff0000"
-						onClick={() => setFilter(new ColorFilter("#ff0000"))}
-					/>
-					<FilterItem
-						tooltip="Zelené pozadie"
-						backgroundColor="#00ff00"
-						onClick={() => setFilter(new ColorFilter("#00ff00"))}
-					/>
-					<FilterItem
-						tooltip="Modré pozadie"
-						backgroundColor="#0000ff"
-						onClick={() => setFilter(new ColorFilter("#0000ff"))}
-					/>
-					<FilterItem
-						tooltip="Žlté pozadie"
-						backgroundColor="#fff000"
-						onClick={() => setFilter(new ColorFilter("#fff000"))}
-					/>
-					<FilterItem
-						tooltip="Biele pozadie"
-						backgroundColor="#ffffff"
-						onClick={() => setFilter(new ColorFilter("#ffffff"))}
-					/>
-					<FilterItem
-						tooltip="Čierne pozadie"
-						backgroundColor="#000000"
-						onClick={() => setFilter(new ColorFilter("#000000"))}
-					/>
-				</FiltersGroup>
-				<FiltersGroup title="Vlastné pozadie">
-					<input
-						type="file"
-						accept="image/*"
-						onChange={({ target: { files } }) => handleFileUpload(files)}
-						style={{ display: "none" }}
-					/>
-					<FilterItem
-						tooltip="Nahrajte si vlasné pozadie"
-						icon={<FileUploadRoundedIcon fontSize="large" />}
-						onClick={() =>
-							image && setFilter(new CustomBackgroundImageFilter(image))
-						}
-					/>
-				</FiltersGroup>
-			</Paper>
-		</Container>
-	);
+  return (
+    <Container>
+      <VideoContainer>
+        {permissionStatus !== undefined && (
+          <Webcam filter={filter} resolution={resolution} />
+        )}
+      </VideoContainer>
+      <Paper elevation={1} style={{ width: 336 }}>
+        <FiltersGroup title="Filtre">
+          <FilterItem
+            tooltip="Odstrániť filtre"
+            icon={<BlockOutlined fontSize="large" />}
+            onClick={() => setFilter(undefined)}
+          />
+          <FilterItem
+            tooltip="Ľahké rozmazanie"
+            icon={<DeblurOutlined fontSize="large" />}
+            onClick={() => setFilter(new BlurFilter(6))}
+          />
+          <FilterItem
+            tooltip="Silné rozmazanie"
+            icon={<BlurOnIcon fontSize="large" />}
+            onClick={() => setFilter(new BlurFilter(12))}
+          />
+        </FiltersGroup>
+        <FiltersGroup title="Farby">
+          <FilterItem
+            tooltip="Červené pozadie"
+            backgroundColor="#ff0000"
+            onClick={() => setFilter(new ColorFilter("#ff0000"))}
+          />
+          <FilterItem
+            tooltip="Zelené pozadie"
+            backgroundColor="#00ff00"
+            onClick={() => setFilter(new ColorFilter("#00ff00"))}
+          />
+          <FilterItem
+            tooltip="Modré pozadie"
+            backgroundColor="#0000ff"
+            onClick={() => setFilter(new ColorFilter("#0000ff"))}
+          />
+          <FilterItem
+            tooltip="Žlté pozadie"
+            backgroundColor="#fff000"
+            onClick={() => setFilter(new ColorFilter("#fff000"))}
+          />
+          <FilterItem
+            tooltip="Biele pozadie"
+            backgroundColor="#ffffff"
+            onClick={() => setFilter(new ColorFilter("#ffffff"))}
+          />
+          <FilterItem
+            tooltip="Čierne pozadie"
+            backgroundColor="#000000"
+            onClick={() => setFilter(new ColorFilter("#000000"))}
+          />
+        </FiltersGroup>
+        <FiltersGroup title="Vlastné pozadie">
+          <Upload
+            accept="image/*"
+            beforeUpload={(file) => {
+              setFilter(new CustomBackgroundImageFilter(file));
+              return file;
+            }}
+          >
+            <FilterItem
+              tooltip="Nahrajte si vlasné pozadie"
+              icon={<FileUploadRoundedIcon fontSize="large" />}
+            />
+          </Upload>
+        </FiltersGroup>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Home;
