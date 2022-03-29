1432e1db79be300a550d3ec33c28e8d45b6c6e88
Adam Vician <adamvician@gmail.com>
2022-03-29 19:52:29 +0200

    Added title, description and favico

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,8 @@ export default class MyDocument extends Document {
 		return (
 			<Html lang="sk">
 				<Head>
-					<link rel="shortcut icon" href="/favicon.png" />
+					<link rel="shortcut icon" href="/favicon.ico" />
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<link
 						rel="stylesheet"
 						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
@@ -13,6 +14,11 @@ export default class MyDocument extends Document {
 					<link
 						rel="stylesheet"
 						href="https://fonts.googleapis.com/icon?family=Material+Icons"
+					/>
+					<title>BG Remover</title>
+					<meta
+						name="description"
+						content="Created with NEXT.js, MUI and MediaPipe Selfie Segmentation. You can apply multiple background filters to your webcam input just like in Google Meet."
 					/>
 				</Head>
 				<body className="antialiased scroll-bar">
